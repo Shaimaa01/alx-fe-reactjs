929c3e54d8138c165ffc26e5a9d0aaf9c4699ec5
shaimaa <shisko939@gmail.com>
2024-11-17 14:27:03 +0200

    fix i do not know what to fix

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -5,10 +5,10 @@ import "./App.css";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-import ProfilePage from './ProfilePage';
-import UserContext from './UserContext';
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import ProfilePage from "./ProfilePage";
+import UserContext from "./UserContext";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -43,8 +43,8 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
       <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
+        <ProfilePage />
+      </UserContext.Provider>
     </>
   );
 }
