9dea9f688d53475a8f7524b48b40e930302ae1fd
shaimaa <shisko939@gmail.com>
2024-11-17 20:54:57 +0200

    3

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,10 @@
+// src/Footer.jsx
+function Footer() {
+    return (
+      <footer style={{ padding: '10px', background: '#333', color: '#fff', textAlign: 'center' }}>
+        <p>&copy; 2024 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
