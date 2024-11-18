efbb95d51b9e0147f9dbce3e545b73381636252e
shaimaa <shisko939@gmail.com>
2024-11-18 13:06:09 +0200

    fix

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './RecipeList';
 import RecipeDetails from './RecipeDetails';
+import EditRecipeForm from './components/EditRecipeForm';
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <EditRecipeForm/>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipe/:id" element={<RecipeDetails />} />
       </Routes>
