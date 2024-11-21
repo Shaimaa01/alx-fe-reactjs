3a557d5baa2229996d933ea9bde76cf16ef776a3
shaimaa <shisko939@gmail.com>
2024-11-21 11:27:46 +0200

    still fix 0

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,16 +1,21 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+
   addRecipe: (newRecipe) =>
-    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+   set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+  setRecipes: (recipes) => set({ recipes }),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
         recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
-  deleteRecipe: (recipeId) =>
+
+    deleteRecipe: (recipeId) =>
     set((state) => ({
       recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
     })),
