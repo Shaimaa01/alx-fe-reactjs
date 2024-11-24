d0324e6ea03dde1e2d917989ecaae1b2c3e80a18
shaimaa <shisko939@gmail.com>
2024-11-24 21:16:48 +0200

    last fix 2

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,31 +1,36 @@
-
-import './App.css'
+import "./App.css";
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Login from './components/Login';
-import UserProfile from './components/UserProfile';
-import ProtectedRoute from './components/ProtectedRoute';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import UserProfile from "./components/UserProfile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import BlogPost from "./components/BlogPost";
 
 function App() {
-
-
   return (
     <>
       <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile/*" element={<Profile />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/user/:userId" element={<UserProfile />} />
-      <Route path="/profile/*" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-    </Routes>
-  </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user/:userId" element={<UserProfile />} />
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/blog/:id" element={<BlogPost />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
