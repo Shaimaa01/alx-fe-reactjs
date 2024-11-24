b7934102a89362cc3121230a243a85bcf43cc486
shaimaa <shisko939@gmail.com>
2024-11-24 21:22:26 +0200

    the end of fixing 2

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -5,8 +5,11 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { isAuthenticated } from '../utils/auth';
 
-const ProtectedRoute = ({ children }) => {
-  return isAuthenticated() ? children : <Navigate to="/login" />;
-};
+function ProtectedRoute({ children }) {
+    // eslint-disable-next-line no-undef
+    const isAuthenticated = useAuth();
+  
+    return isAuthenticated ? children : <Navigate to="/login" />;
+  }
 
 export default ProtectedRoute;
