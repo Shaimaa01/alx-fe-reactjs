07c99ef9d5cddc450058cbbec3ec2987cafa5878
shaimaa <shisko939@gmail.com>
2024-12-15 04:07:00 +0200

    fix task1

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,49 +1,12 @@
-import { useState } from "react";
 import Search from "./components/Search";
-import { fetchUserData } from "./services/githubService";
 
 const App = () => {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  const handleSearch = async (username) => {
-    setLoading(true);
-    setError("");
-    setUser(null);
-
-    try {
-      const data = await fetchUserData(username);
-      setUser(data);
-    } catch {
-      setError("Looks like we can't find the user");
-    } finally {
-      setLoading(false);
-    }
-  };
-  console.log(user)
-
   return (
-    <div>
+    <>
       <h1>GitHub User Search Application</h1>
       <p>Search for GitHub profiles here!</p>
-      <Search onSearch={handleSearch} />
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-      {user && (
-        <div>
-          <img
-            src={user.avatar_url}
-            alt={`${user.login}'s avatar`}
-            width="100"
-          />
-          <h2>{user.name || user.login}</h2>
-          <a href={user.html_url} target="_blank" rel="noopener noreferrer">
-            View Profile
-          </a>
-        </div>
-      )}
-    </div>
+      <Search />
+    </>
   );
 };
 
--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,28 +1,62 @@
-/* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import { useState } from "react";
+import { fetchUserData } from "../services/githubService";
 
-// eslint-disable-next-line react/prop-types
-const Search = ({ onSearch }) => {
-    const [username, setUsername] = useState('');
+const Search = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [username, setUsername] = useState("");
+  const [data, setUserData] = useState("");
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSearch(username);
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleSearch(username);
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    placeholder="Enter GitHub username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-                <button type="submit">Search</button>
-            </form>
-        </div>
-    );
+  const handleSearch = async (username) => {
+    setLoading(true);
+    setError("");
+
+    try {
+      const data = await fetchUserData(username);
+      setUserData(data);
+    } catch {
+      setError("Looks like we can't find the user");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter GitHub username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+      <div>
+        {loading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+        {data && (
+          <div>
+            <img
+              src={data.avatar_url}
+              alt={`${data.login}'s avatar`}
+              width="100"
+            />
+            <h2>{data.name || data.login}</h2>
+            <a href={data.html_url} target="_blank" rel="noopener noreferrer">
+              View Profile
+            </a>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Search;
